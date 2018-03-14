d62a6f463ce24ab5e673396e85e2df017e73197c
Garrett Quintero <garrett.quintero@gmail.com>
2018-03-14 13:58:50 -0600

    started making ToDos for Garrett

--- app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListActivity.java
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListActivity.java
@@ -11,3 +11,14 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
     }
 }
+
+//TODO: create list scrolling activity similar to the one from CriminalIntent
+//TODO: add a static "sort by" button to the scrolling page
+//TODO: add a + menu item to add a new task
+//TODO: create new ToDoTask class
+//TODO: create new TaskWithImage class that extends ToDoTask
+//TODO: create a TaskActivity to show the details of the activity
+//TODO: give TaskActivity an Edit/Done button
+//TODO: set up TaskActivity to "be a child" of TaskListActivity so that the up button takes it back
+
+//TODO: set up a landscape view for TaskListActivity that shows the list and the details like CriminalActivity did
