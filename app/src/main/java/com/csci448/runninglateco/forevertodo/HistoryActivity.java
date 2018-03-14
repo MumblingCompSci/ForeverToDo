fa1852848c33c2aff4ec67ea634af90fc97db1c8
Garrett Quintero <garrett.quintero@gmail.com>
2018-03-14 14:05:50 -0600

    Created HistoryActivity and added some ToDos

---
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/HistoryActivity.java
@@ -0,0 +1,18 @@
+package com.csci448.runninglateco.forevertodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
+
+//TODO: set up a view(or fragment?) for the completion history graph
+//TODO: set up the views for the text about the completion statistics
+//TODO: create the scrolling fragment for recently completed tasks
+//TODO: toolbar???????
