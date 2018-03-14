beade804e2d179464b162f4b4533f80a7e7876d8
Garrett Quintero <garrett.quintero@gmail.com>
2018-03-14 14:13:48 -0600

    added CompletedFragment

---
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/CompletedFragment.java
@@ -0,0 +1,10 @@
+package com.csci448.runninglateco.forevertodo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by quintero on 3/14/18.
+ */
+
+public class CompletedFragment extends Fragment {
+}
