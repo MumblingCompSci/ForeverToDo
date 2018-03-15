aa5b558870b174f2f8ed7bf10377fc82dfee0503
Garrett Quintero <garrett.quintero@gmail.com>
2018-03-15 00:16:55 -0600

    created ToDoTask and ToDoWithImage

---
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/ToDoTask.java
@@ -0,0 +1,20 @@
+package com.csci448.runninglateco.forevertodo;
+
+import java.util.Date;
+
+/**
+ * Created by quintero on 3/15/18.
+ */
+
+public class ToDoTask {
+    private String mTitle;
+    private String mDescription;
+
+
+    //TODO: how do we want to store dates and times?
+    /*private Date mDueDate;
+    private Date mCompleteDate;*/
+
+    //TODO: do we want to create a Builder for this class?  I don't know how practical that'd be but they look cool
+    // We probably don't actually need to make this class for the Alpha release
+}
---
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/ToDoWithImage.java
@@ -0,0 +1,8 @@
+package com.csci448.runninglateco.forevertodo;
+
+/**
+ * Created by quintero on 3/15/18.
+ */
+
+public class ToDoWithImage extends ToDoTask {
+}
