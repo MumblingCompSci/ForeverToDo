df3d2ef44836daba16c7614a7e0134cdcae4ed44
Tracy Karol <tkarol@spotxchange.com>
2018-03-15 12:53:27 -0600

    Added some comments

--- app/src/main/java/com/csci448/runninglateco/forevertodo/HistoryActivity.java
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/HistoryActivity.java
@@ -17,5 +17,9 @@ public class HistoryActivity extends AppCompatActivity {
 //TODO: create the scrolling fragment for recently completed tasks
 //TODO: toolbar???????
 //TODO: implement Navigation Drawer????
-
+/*I'm cool with whichever kind of menu thing you wanna use!
 //TODO: should this be a SingleFragmentActivity or something else??
+/*It depends on whether you want to have the history page be a single fragment with all of its components on one layout or if you
+ * want to have multiple fragments, like have one fragment show the chart, and then have a button that says see completed tasks that
+  * triggers an intent for a fragment that has a list of the completed tasks. If you have multiple fragments, then it wouldn't be a Single
+  * FragmentActivity, so it's really up to you on how you want this section to work.*/
