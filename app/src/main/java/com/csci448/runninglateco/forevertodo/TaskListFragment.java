5214273d1eaef6872b648206aa0d52e4f439500d
Tracy Karol <tkarol@spotxchange.com>
2018-03-16 16:45:04 -0600

    Edited TaskListFragment to be a recycler view.

--- app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListFragment.java
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListFragment.java
@@ -16,6 +16,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 /**
@@ -24,30 +26,33 @@ import java.util.List;
 
 public class TaskListFragment extends Fragment {
     private static final String TAG = "TaskListFragment";
-
+    private RecyclerView mRecyclerView;
+    private TaskAdapter mTaskAdapter;
     private Button mTaskButton;
     private Button mHistoryButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.task_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        mTaskButton = (Button) view.findViewById(R.id.to_task_button);
-        mTaskButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ToDoTask testTask = new ToDoTask();
-                ((TaskListActivity) getActivity()).onTaskSelected(testTask);
-            }
-        });
-        mHistoryButton = (Button) view.findViewById(R.id.to_history_button);
-        mHistoryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startHistory = new Intent(getActivity(), HistoryActivity.class);
-                (getActivity()).startActivity(startHistory);
-            }
-        });
+//        mTaskButton = (Button) view.findViewById(R.id.to_task_button);
+//        mTaskButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ToDoTask testTask = new ToDoTask();
+//                ((TaskListActivity) getActivity()).onTaskSelected(testTask);
+//            }
+//        });
+//        mHistoryButton = (Button) view.findViewById(R.id.to_history_button);
+//        mHistoryButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent startHistory = new Intent(getActivity(), HistoryActivity.class);
+//                (getActivity()).startActivity(startHistory);
+//            }
+//        });
 
 
         return view;
@@ -59,7 +64,58 @@ public class TaskListFragment extends Fragment {
     public static TaskListFragment newInstance() {
         return new TaskListFragment();
     }
-}
 
+
+    private class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private ToDoTask mTask;
+
+        public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_task, parent, false));
+            itemView.setOnClickListener(this);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.task_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.task_due_date);
+        }
+
+        public void bind(ToDoTask task) {
+            mTask = task;
+            mTitleTextView.setText(mTask.getTitle());
+            mDateTextView.setText(mTask.getCompleteDate().toString());
+        }
+
+        @Override
+        public void onClick(View view){
+
+        }
+
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder>{
+        private List<ToDoTask> mTasks;
+
+        public TaskAdapter(List<ToDoTask> tasks){
+            mTasks = tasks;
+        }
+
+        @Override
+        public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new TaskHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(TaskHolder holder, int position){
+            ToDoTask task = mTasks.get(position);
+            holder.bind(task);
+        }
+
+        @Override
+        public int getItemCount(){
+            return mTasks.size();
+        }
+    }
+
+}
 
 //TODO: make this a recylcerview that actually is a list of tasks
