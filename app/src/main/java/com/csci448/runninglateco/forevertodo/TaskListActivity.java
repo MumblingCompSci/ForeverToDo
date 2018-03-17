9342d8f048fce0051b0da0e6e682f4df5bfc187c
Garrett Quintero <garrett.quintero@gmail.com>
2018-03-16 20:26:32 -0600

    set up dummy tasks

--- app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListActivity.java
+++ app/src/main/java/com/csci448/runninglateco/forevertodo/TaskListActivity.java
@@ -2,21 +2,36 @@ package com.csci448.runninglateco.forevertodo;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class TaskListActivity extends SingleFragmentActivity
+public class TaskListActivity extends AppCompatActivity
     implements TaskListFragment.Callbacks{
     private static final String TAG = "TaskListActivity";
 
-    @Override
     protected Fragment createFragment() {
         return TaskListFragment.newInstance();
     }
 
-    @Override
     protected int getLayoutResId() { return R.layout.activity_masterdetail; }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
 
     @Override
     public void onTaskSelected(ToDoTask task){
